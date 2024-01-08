7933ba7c6cc069d28776636e84bb5d650931040b
rhino550 <155978661+rhino550@users.noreply.github.com>
2024-01-08 19:16:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 //Set Request Size Limit
-app.use(express.limit(100000000));
+// app.use(express.limit(100000000));
 
 app.use('', (_, res, next) => {
     res.set('Cross-Origin-Resource-Policy', 'cross-origin');
